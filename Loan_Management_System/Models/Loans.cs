4fff7968864f3d6d2a5945523793cb5fe6b5ec28
Saikiran reddy <saikiran.medhimale@gmail.com>
2022-11-03 14:32:36 +0530

    models commit

---
+++ Loan_Management_System/Models/Loans.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loan_Management_System.Models
+{
+    public class Loans
+    {
+        [Key]
+        public string Type { get; set; }
+
+        [Required]
+        public string InterestRate { get; set; }
+    }
+}
