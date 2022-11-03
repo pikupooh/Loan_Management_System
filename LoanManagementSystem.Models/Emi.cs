900c9974bededda4632de8b32f99f623676a5825
Saikiran reddy <saikiran.medhimale@gmail.com>
2022-11-03 18:27:55 +0530

    added models

---
+++ LoanManagementSystem.Models/Emi.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Loan_Management_System.Models
+{
+    public class Emi
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Amount { get; set; }
+
+        public string CustomerID { get; set; }
+    }
+}
