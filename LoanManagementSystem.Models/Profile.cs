27857d8675d7e9ec567b3eea8947f1c3b3c91d5c
Saikiran reddy <saikiran.medhimale@gmail.com>
2022-11-04 10:41:01 +0530

    Added validations

--- LoanManagementSystem.Models/Profile.cs
+++ LoanManagementSystem.Models/Profile.cs
@@ -10,14 +10,22 @@ namespace Loan_Management_System.Models
 
         [Required]
         public string Name { get; set; }
-        
-        [Required]
+
+        [Display(Name = "Email address:")]
+        [Required(ErrorMessage = "The email address is required")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
 
-        [Required]
+        [Display(Name = "PAN Card:")]
+        [Required(ErrorMessage = "PAN Number is required")]
+        [RegularExpression("^([A-Za-z]){5}([0-9]){4}([A-Za-z]){1}$", ErrorMessage = "Invalid PAN Number")]
         public string PAN { get; set; }
 
+        [Display(Name = "Phone Number:")]
+        [Required(ErrorMessage = "Phone Number is required")]
+        [RegularExpression("^([0-9]){10}$", ErrorMessage = "Invalid Phone Number")]
         public int PhoneNumber { get; set; }
+        
         public string Address { get; set; }
 
         public List<Emi> Emis { get; set; }
